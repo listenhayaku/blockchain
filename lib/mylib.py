c6db7a6eedd88a86d9981cf9774766a4084a0db4
listenhayaku <hhc8811105@ccit.ndu.edu.tw>
2023-04-21 10:26:11 +0800

    OK

--- lib/mylib.py
+++ lib/mylib.py
@@ -1,4 +1,6 @@
 import msvcrt
+import select
+import sys
 
 class myNonBlockingInput():
     def __init__(self):
@@ -13,4 +15,27 @@ class myNonBlockingInput():
             self.buffer = ""
             return self.ret
         else: return ""
+
+
+'''
+class myNonBlockingInput():
+    def __init__(self):
+        self.buffer = str()
+        self.ret = str()
+
+    def ReceiveInput(self):
+        i, o, e = select.select( [sys.stdin], [], [], 1)
+        if (i):
+            self.buffer.append(sys.stdin.buffer.readline().strip())   #sys.stdin.readline() 可以讀取成str
+#sys.stdin.buffer.readline() 讀取成bytes
+        else:
+            pass
+        while len(self.buffer) > 0:
+            self.ret = bytes(self.buffer.pop())
+            print("(debug)[communicator.py][inputFunc]self.ret:",self.ret)
+        return self.ret
+        #print("(debug)[communicator.py][[testFunc]dataQueue:",dataQueue)
+        #大感謝這個方法使input可以被中斷出來檢查flag是否轉為停止
+        #https://stackoverflow.com/questions/1335507/keyboard-input-with-timeout -Pontus
+'''
     
